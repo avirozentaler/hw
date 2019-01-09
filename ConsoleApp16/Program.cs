a95b7f9eb46c6282807662a82f7e27403f98bcf9
avirozentaler <46267593+avirozentaler@users.noreply.github.com>
2019-01-10 01:56:45 +0200

    Add files via upload

---
+++ ConsoleApp16/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= size; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write(j);
+                }
+                Console.WriteLine();
+            }
+             for (int i = size-1; i >=1; i--)
+             {
+                 for (int j = 1; j <= i; j++)
+                 {
+                     Console.Write(j);
+                 }
+                 Console.WriteLine();
+        }
+            }
+        }
+    }
